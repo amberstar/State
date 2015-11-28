a9e081a96479c24d029e230125dfc9625959a809
Amber Star <amber@simpletouchsoftware.com>
2015-11-28 14:04:16 -0500

    Update Dissolver - support for relationship transforms

--- STState/Transforms/DissolverTransform.swift
+++ STState/Transforms/DissolverTransform.swift
@@ -6,7 +6,8 @@
 
 public final class DissolverTransform<T: protocol<Monoid, CollectionType>>: ValueTransform {
     public typealias ValueType = T
-    public typealias TransformedType = T
+    public typealias TransformedType = ValueType
+    public typealias DecodedType = ValueType
     
     public static func  transform(value: ValueType?) -> TransformedType? {
         if let value = value where value.count > 0 {
@@ -15,8 +16,8 @@ public final class DissolverTransform<T: protocol<Monoid, CollectionType>>: Valu
         return nil
     }
     
-    public static func reverseTransform(value: AnyObject?) -> ValueType? {
-        if let value = value as? T {
+    public static func reverseTransform(value: DecodedType?) -> ValueType? {
+        if let value = value {
             return value
         }
         return T.mempty
--- STState/Transforms/ValueTransform.swift
+++ STState/Transforms/ValueTransform.swift
@@ -1,7 +1,8 @@
 public protocol ValueTransform {
     typealias ValueType
     typealias TransformedType
+    typealias DecodedType
     
     static  func transform(value: ValueType?) -> TransformedType?
-    static  func reverseTransform(value: AnyObject?) -> ValueType?
+    static  func reverseTransform(value: DecodedType?) -> ValueType?
 }
