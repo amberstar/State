67183bf1357fa8fc035903258e1efef8f3ed3cc6
Amber Star <amber@simpletouchsoftware.com>
2016-07-20 11:03:26 -0400

    [DOCUMENT] revision

--- Sources/Format.swift
+++ Sources/Format.swift
@@ -1,9 +1,28 @@
-//
-//  State: Format.swift
-//
-//  Created by Amber Star on 7/17/16.
-//  Copyright © 2016 Amber Star. All rights reserved.
-//
+
+/*
+Copyright (c) 2016 SIMPLETOUCH LLC
+
+The MIT License (MIT)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+---
+*/
 
 /// Format - File formats for Storage
 ///
@@ -20,7 +39,7 @@ public enum Format {
     case plist
     /// json format
     case json
-    
+
     /// returns a formatter that can
     /// read and write to the current format.
     var formatter : Formatter {
@@ -39,7 +58,7 @@ public enum Format {
 
 /// A base formatter that provides the binary format.
 class Formatter {
-    
+
     /// write data to a file
     /// - returns: true if succeeded, false if failed
     func write(_ object: AnyObject, to url: URL) -> Bool  {
@@ -50,14 +69,14 @@ class Formatter {
             return false
         }
     }
-    
+
     /// write data to  NSData
     /// - returns: NSData or nil if failed
     func makeData(from object: AnyObject,
                   prettyPrint: Bool) -> Data?  {
         return NSKeyedArchiver.archivedData(withRootObject: object)
     }
-    
+
     /// write data to String
     /// - returns: a string or nil if failed
     func makeString(from object: AnyObject) -> String? {
@@ -67,14 +86,14 @@ class Formatter {
             return nil
         }
     }
-    
+
     /// Read data from NSData
     /// - returns: a data object or nil
     func read(_ data: Data) -> AnyObject? {
         return NSKeyedUnarchiver.unarchiveObject(with: data) as? [String : AnyObject]
     }
-    
-    
+
+
     /// Read file from a URL
     /// - returns: a data object or nil
     func read(_ url: URL) -> AnyObject? {
@@ -83,14 +102,14 @@ class Formatter {
         }
         return nil
     }
-    
+
     /// Read data from a string
     /// - returns: a data object or nil
     func read(_ content: String) -> AnyObject? {
         guard let data = makeData(from: content) else { return nil }
         return read(data)
     }
-    
+
     func makeData(from string : String) -> Data? {
         return string.data(using: String.Encoding.utf8,
                            allowLossyConversion: true)
@@ -99,7 +118,7 @@ class Formatter {
 
 /// JSON serialization format
 final class JSONFormatter: Formatter {
-    
+
     override func read(_ data: Data) -> AnyObject? {
         do {
             let o: AnyObject =  try JSONSerialization.jsonObject(
@@ -110,22 +129,22 @@ final class JSONFormatter: Formatter {
             return nil
         }
     }
-    
+
     override func makeData(from object: AnyObject,
                            prettyPrint: Bool) -> Data?  {
-        
+
         guard JSONSerialization.isValidJSONObject(object) else {
             return nil
         }
-        
+
         let options: JSONSerialization.WritingOptions = prettyPrint ? .prettyPrinted : []
         let data: Data?
-        
+
         do {
             data = try JSONSerialization.data(withJSONObject: object, options: options)
         }
         catch let error as NSError {
-            
+
             Swift.print(error)
             data = nil
         }
@@ -135,45 +154,45 @@ final class JSONFormatter: Formatter {
 
 /// XML Plist serialization format
 final class PlistFormatter: Formatter {
-    
+
     override func read(_ data: Data) -> AnyObject? {
-        
+
         do {
             let o: AnyObject =
                 try PropertyListSerialization.propertyList(
                     from: data, options:[.mutableContainersAndLeaves], format:nil
             )
-            
+
             return o
         } catch let error as NSError {
-            
+
             Swift.print(error)
             return nil
         }
     }
-    
+
     override func read(_ content: String) -> AnyObject? {
         let s = content as NSString
         return s.propertyList()
     }
-    
-    
+
+
     override func makeData(from object: AnyObject,
                            prettyPrint: Bool) -> Data?  {
-        
+
         guard PropertyListSerialization.propertyList(
             object, isValidFor: PropertyListSerialization.PropertyListFormat.xml) else {
                 return nil
         }
-        
+
         do {
             let data: Data? = try PropertyListSerialization.data(
                 fromPropertyList: object, format: PropertyListSerialization.PropertyListFormat.xml, options: .allZeros
             )
-            
+
             return data
         } catch let error as NSError {
-            
+
             Swift.print(error)
         }
         return nil
--- Sources/Store.swift
+++ Sources/Store.swift
@@ -1,67 +1,87 @@
-//
-// State: Store.swift
-// Copyright © 2016 SIMPLETOUCH LLC. All rights reserved.
-//
-// Licensed under The MIT License (MIT)
-//
+
+/*
+Copyright (c) 2016 SIMPLETOUCH LLC
+
+The MIT License (MIT)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+---
+*/
 
 import Foundation
 import UIKit
 
 /*
 1. A `Store` is an abstract key-value property store for
-    reading a writing values. 
- 
+    reading a writing values.
+
 2. A`Formatter` is used internally by a Store to read and write it's
    data to files, strings, data.
 */
 
 public struct Store {
-    
+
     /// The contents of the store.
     public var data : [String : AnyObject]
-    
+
     /// Create an instance with initial data.
     public init(data: [String : AnyObject] = [:]) {
         self.data = data
     }
-    
+
 // MARK: - Values
-    
+
     /// Returns the value associated with the specified key.
     public func value<V>(forKey key: String) -> V? {
         return data[key] as? V
     }
-    
+
     /// Returns the object associated with the specified key.
     public func object(forKey key: String) -> AnyObject? {
         return data[key]
     }
-    
+
     /// Returns the array associated with the specified key.
     public func array(forKey key: String) -> [AnyObject]? {
         return data[key] as? [AnyObject]
     }
-    
+
     /// Returns the dictionary associated with the specified key.
     public func dictionary(forKey key: String) -> [String : AnyObject]? {
         return data[key] as? [String : AnyObject]
     }
-    
+
     /// Sets the value of the specified key to the specified value.
     public mutating func set<V>(_ value: V?, forKey key: String) {
         guard let value = value else { return }
         data[key] = value as? AnyObject
     }
-    
+
 // MARK: - URL
-    
+
     /// Returns the url associated with the specified key.
     public func value(forKey key: String) -> URL? {
         guard let urlString : String  = value(forKey: key) else { return nil }
         return URL(string: urlString)
     }
-    
+
     /// Sets the value of the specified key to the specified url value.
     public mutating func set(_ value: URL, forKey key: String) {
         guard let urlString : String  = value.absoluteString else { return }
@@ -69,7 +89,7 @@ public struct Store {
     }
 
 // MARK: - UIColor
-    
+
     /// Returns the color associated with the specified key.
     public func value(forKey key: String) -> UIColor? {
         guard
@@ -81,33 +101,33 @@ public struct Store {
         else {
             return nil
         }
-        
+
         return UIColor(colorLiteralRed: red, green: green, blue: blue, alpha: alpha)
     }
-    
+
     /// Sets the value of the specified key to the specified color value.
     public mutating func set(_ value: UIColor, forKey key: String) {
         var red, green, blue, alpha  : CGFloat
         red = 0; green = 0; blue = 0; alpha = 0
         value.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
-        
+
         var cStore = Store()
         cStore.set(red, forKey: "red")
         cStore.set(green, forKey: "green")
         cStore.set(blue, forKey: "blue")
         cStore.set(alpha, forKey: "alpha")
-        
+
         set(cStore, forKey: key)
     }
-    
-    
-// MARK: - Bool 
-    
+
+
+// MARK: - Bool
+
     /// Returns the bool value associated with the specified key.
     public func bool(forKey key: String) -> Bool? {
         return value(forKey: key)
     }
-    
+
     /// Returns the bool value associated with the specified key
     /// or the specified defaultValue if key is not found.
     public func bool(forKey key: String, defaultValue: Bool) -> Bool {
@@ -115,63 +135,63 @@ public struct Store {
     }
 
 // TODO: GYB other types.
-    
+
 // MARK: - Sub Stores
-    
+
     /// Returns the sub store associated with the specified key.
     public func store(forKey key: String) -> Store? {
         guard let data = data[key] as? [String : AnyObject] else { return nil }
         return Store(data: data)
     }
-    
+
     /// Sets the value of the specified key to the specified sub store.
     public mutating func set(_ store: Store, forKey key: String) {
         data[key] = store.data
     }
-    
+
 // MARK: File
-    
+
     /// Create a store with file content in the specified format.
     public init?(file: URL, format: Format) {
         guard let data = format.formatter.read(file) as? [String : AnyObject] else { return nil }
         self.data = data
     }
-    
+
     /// Writes the store content to a file in the specified format.
     ///
     /// - Returns: `true` if succeeded otherwise `false`
     public func write(to file: URL, format: Format) -> Bool {
         return format.formatter.write(data, to: file)
     }
-    
+
 // MARK: String
-    
-    /// Returns the stores content as a string in the 
+
+    /// Returns the stores content as a string in the
     /// specified format.
     public func makeString(format: Format) -> String? {
         return format.formatter.makeString(from: data)
     }
-    
+
     /// Create a store with a string in the specified format.
     public init?(content: String, format: Format) {
         guard let data = format.formatter.read(content) as? [String : AnyObject] else { return nil }
         self.data = data
     }
-    
+
 // MARK: Data
-    
+
     /// Create a store with data in the specified format.
     public init?(content: Data, format: Format) {
         guard let data = format.formatter.read(content) as? [String : AnyObject] else { return nil }
         self.data = data
     }
-    
+
     public func makeData(format: Format) -> Data? {
         return format.formatter.makeData(from: data, prettyPrint: true)
     }
-    
+
 // MARK: Removal
-    
+
     /// Removes and returns the value at the specified key
     /// or .none if not found.
     public mutating func remove(key: String) -> AnyObject? {
